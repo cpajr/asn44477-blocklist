8caf44214fdb709271af799f735784b9577e77a9
Charles Allen <charles.allen@zagg.com>
2024-06-05 12:27:33 -0600

    Add timestamp to output

--- asn44477-prefixes.py
+++ asn44477-prefixes.py
@@ -1,12 +1,16 @@
 import bgpstuff
 import ipaddress
 import sys
+from datetime import datetime
 
 c = bgpstuff.Client()
 c.get_sourced_prefixes(44477)
 
+timestamp = datetime.now()
+timestamp_str = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+output = f'# Generated {timestamp_str} \n'
+
 if c.sourced:
-    output = ""
     for prefix in c.sourced:
         if ipaddress.ip_network(prefix).version == 4:
             output += str(prefix) + "\n"
