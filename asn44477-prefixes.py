d98cb1c526f707b8f8965135c36d8db8724a4af6
Charles Allen <charles.allen@zagg.com>
2024-06-05 09:53:21 -0600

    add script and Github workflow

---
+++ asn44477-prefixes.py
@@ -0,0 +1,13 @@
+import bgpstuff
+import ipaddress
+import sys
+
+c = bgpstuff.Client()
+c.get_sourced_prefixes(44477)
+
+if c.sourced:
+    output = ""
+    for prefix in c.sourced:
+        if ipaddress.ip_network(prefix).version == 4:
+            output += prefix + "\n"
+    sys.stdout.write(output)
